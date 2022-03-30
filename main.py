3f8b2db778b8fa24f0da2c55733bb58de142fa9b
Dani643 <gonzalezrodriguez.daniel@gmail.com>
2022-03-30 18:03:31 +0200

    GitIgnore

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import files
 
 
 if __name__ == '__main__':
-    # Número de meses a analizar
-    meses_analizar = 12
-    # Se configura si se quiere recuperar el resumen de cada capítulo
+    # Número de meses a analizar. 1200 meses es más del máximo para llegar a la primera serie
+    meses_analizar = 1200
+    # Se configura si se quiere recuperar el resumen de cada capítulo. El número de conexiones si se omite
+    # se reducirá ampliamente
     resumen_capitulo = 0
     # Nombre de las direcciones web necesarias
     url_base = 'https://www.pogdesign.co.uk'
@@ -27,5 +28,4 @@ if __name__ == '__main__':
         print('Se encontraron problemas durante el proceso y se analizó {} meses'.format(i))
     if resumen_capitulo == 1:
         scraping.episode_summary(series, url_base)
-    print(series)
     files.save_dictionary(series, resumen_capitulo)
